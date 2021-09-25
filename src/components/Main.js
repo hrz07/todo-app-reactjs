8a14787237cc6aeec14844b0f0373ea25dcbe75f
Rashedul Islam Hridoy <hridoy.developer@yahoo.com>
2021-09-25 23:22:07 +0600

    Main.js Updated
    Updated Main.js with Bootstrap

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './MainStyle.css'
 
 const Main = () => {
     return (
         <div>
-            <div className="createSection d-flex flex-column m-auto mt-5 p-5 w-50">
+            <div className="createSection d-flex flex-column m-auto mt-5 p-4 w-50 shadow p-3 mb-5 bg-body rounded">
                 <div className="header">
-                    <h1>MY TODO APP</h1>
+                    <h1 className='fw-bold'>MY TODO APP</h1>
                 </div>
                 <div className="inputSection d-flex m-auto ">                   
-                    <input  className='form-control w-100 ' type="text" placeholder='Enter Task' />
+                    <input  className='form-control  mx-3 border border-primary border-2' type="text" placeholder='Enter Task' />
                     <button className='btn btn-primary'>ADD</button>
                 </div>               
             </div>
